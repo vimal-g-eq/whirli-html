113b3f6db0122b6ad57ee2cf88ef062d7474ab74
vimal-g-eq <vimal.gandhi@equestsolutions.net>
2024-09-25 10:10:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-const carousel = document.querySelector('.carousel');
-const carouselInner = document.querySelector('.carousel-inner');
-const carouselItems = document.querySelectorAll('.carousel-item');
-const prevButton = document.querySelector('.carousel-control.prev');
-const nextButton = document.querySelector('.carousel-control.next');
-
-let currentSlide = 0;
-
-prevButton.addEventListener('click', () => {
-  currentSlide--;
-  if (currentSlide < 0) {
-    currentSlide = carouselItems.length - 1;
-  }
-  updateCarousel();
-});
-
-nextButton.addEventListener('click', () => {
-  currentSlide++;
-  if (currentSlide >= carouselItems.length) {
-    currentSlide = 0;
-  }
-  updateCarousel();
-});
-
-function updateCarousel() {
-  carouselInner.style.transform = `translateX(${currentSlide * -100}%)`;
-}
-
-updateCarousel();
+$(".js-carousel-track").slick({
+    centerMode: true,
+    centerPadding: "100px",
+    slidesToShow: 3,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          centerMode: true,
+          centerPadding: "100px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 967,
+        settings: {
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
