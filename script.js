f934bfcc978cc7ad5a1287e6b161e7dda7b23857
vimal-g-eq <vimal.gandhi@equestsolutions.net>
2024-09-30 12:21:32 +0530

    sep-30

--- script.js
+++ script.js
@@ -1,35 +1,200 @@
 $(".js-carousel-track").slick({
-    centerMode: true,
-    centerPadding: "100px",
-    slidesToShow: 3,
-    dots: false,
-    arrows: false,
-    infinite: true,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1440,
-        settings: {
-          centerMode: true,
-          centerPadding: "100px",
-          slidesToShow: 3,
-        },
+  centerMode: true,
+  centerPadding: "100px",
+  slidesToShow: 3,
+  dots: false,
+  arrows: false,
+  infinite: true,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        centerMode: true,
+        centerPadding: "100px",
+        slidesToShow: 3,
       },
-      {
-        breakpoint: 967,
-        settings: {
-          centerMode: true,
-          centerPadding: "40px",
-          slidesToShow: 2,
-        },
+    },
+    {
+      breakpoint: 967,
+      settings: {
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 2,
       },
-      {
-        breakpoint: 480,
-        settings: {
-          centerMode: true,
-          centerPadding: "40px",
-          slidesToShow: 1,
-        },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
       },
-    ],
-  });
+    },
+  ],
+});
+
+// shpo bt brand
+
+$(".js-carousel-track-shop").slick({
+  centerMode: true,
+  centerPadding: "140px",
+  slidesToShow: 5,
+  dots: false,
+  arrows: true,
+  infinite: true,
+  slidesToScroll: 1,
+  prevArrow: $(".js-ingredients-prev-shop"),
+  nextArrow: $(".js-ingredients-next-shop"),
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        centerMode: true,
+        centerPadding: "100px",
+        slidesToShow: 5,
+      },
+    },
+    {
+      breakpoint: 1240,
+      settings: {
+        centerMode: true,
+        centerPadding: "100px",
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 967,
+      settings: {
+        centerMode: true,
+        centerPadding: "50px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 2,
+      },
+    },
+  ],
+});
+
+// new slider
+$(".js-carousel-track-new").slick({
+  centerMode: true,
+  centerPadding: "100px",
+  slidesToShow: 3,
+  dots: false,
+  arrows: true,
+  infinite: true,
+  slidesToScroll: 1,
+  prevArrow: $(".js-ingredients-prev-new"),
+  nextArrow: $(".js-ingredients-next-new"),
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        centerMode: true,
+        centerPadding: "100px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 967,
+      settings: {
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+// shop by name 
+$(".js-carousel-track-shop-by-play").slick({
+  centerMode: true,
+  centerPadding: "100px",
+  slidesToShow: 3,
+  dots: false,
+  arrows: true,
+  infinite: true,
+  slidesToScroll: 1,
+  prevArrow: $(".js-ingredients-prev-shop-by-play"),
+  nextArrow: $(".js-ingredients-next-shop-by-play"),
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        centerMode: true,
+        centerPadding: "100px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 967,
+      settings: {
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+//  real reaviwe from real parents
+
+$(".js-carousel-track-review").slick({
+  centerMode: true,
+  centerPadding: "100px",
+  slidesToShow: 3,
+  dots: false,
+  arrows: true,
+  infinite: true,
+  slidesToScroll: 1,
+  prevArrow: $(".js-ingredients-prev-review"),
+  nextArrow: $(".js-ingredients-next-review"),
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        centerMode: true,
+        centerPadding: "100px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 967,
+      settings: {
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
